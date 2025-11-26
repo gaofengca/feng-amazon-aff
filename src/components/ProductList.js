f3be11308859a2faf2b96fdf746c1dbcbc382cd2
Feng Gao <gaofeng.cloud@gmail.com>
2025-11-25 22:28:20 -0500

    Add TP-Link TL-SG105S-M2 Multi-Gigabit Switch Amazon product to featured products.

---
+++ src/components/ProductList.js
@@ -0,0 +1,10 @@
+const products = [
+  // Previous sample products
+  { id: 1, name: 'Sample Product 1', price: 29.99 },
+  { id: 2, name: 'Sample Product 2', price: 39.99 },
+  { id: 3, name: 'Sample Product 3', price: 49.99 },
+  // New TP-Link product
+  { id: 4, name: 'TP-Link TL-SG105S-M2 Multi-Gigabit Switch', price: 79.99 }
+];
+
+export default products;
