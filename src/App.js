5304cf98a2dc89426785ccf28403fe032c129407
Feng Gao <gaofeng.cloud@gmail.com>
2025-11-25 22:22:16 -0500

    Initial commit for App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProductList from './components/ProductList';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <ProductList />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
